976e04cb9a35ef88848941ac4a074cb137400f39
nour-atef04 <nourmohamedatef04@gmail.com>
2026-05-23 23:49:27 +0300

    fix: fix index css file declaration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// @ts-expect-error: side-effect import of CSS file declaration
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
