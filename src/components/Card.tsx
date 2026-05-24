5199fcb5f2c615cda2b46aacd85540560960ff5d
nour-atef04 <nourmohamedatef04@gmail.com>
2026-05-24 21:50:45 +0300

    fix: fix animation glitches

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,24 +3,31 @@ import { ReactNode, useEffect, useRef, useState } from "react";
 type CardProps = {
   children: ReactNode;
   slide?: "right" | "up";
-  lightBg?: boolean
+  lightBg?: boolean;
 };
 
-export default function Card({ children, slide = "right", lightBg = false}: CardProps) {
+export default function Card({
+  children,
+  slide = "right",
+  lightBg = false,
+}: CardProps) {
   const [isVisible, setIsVisible] = useState(false);
   const domRef = useRef<HTMLLIElement>(null);
 
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        // When the element enters the screen -> set it to visible
-        if (entry.isIntersecting) {
-          setIsVisible(true);
-          // Stop observing once it appears
-          if (domRef.current) observer.unobserve(domRef.current);
-        }
-      });
-    });
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          // When the element enters the screen -> set it to visible
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+            // Stop observing once it appears
+            if (domRef.current) observer.unobserve(domRef.current);
+          }
+        });
+      },
+      { threshold: 0.1 },
+    );
 
     if (domRef.current) observer.observe(domRef.current);
     return () => observer.disconnect();
@@ -29,10 +36,13 @@ export default function Card({ children, slide = "right", lightBg = false}: Card
   return (
     <li
       ref={domRef}
-      className={`hover:border-accent-border border rounded-lg border-divider p-4 flex flex-col gap-4 transition-all duration-1000 ease-out transform ${lightBg && "bg-surface"} ${
-        isVisible &&
-        (slide === "right" ? "animate-slide-right" : "animate-slide-up")
-      }`}
+      className={`hover:border-accent-border border rounded-lg border-divider p-4 flex flex-col gap-4 transform 
+        ${!isVisible ? "opacity-0" : ""} 
+        ${lightBg && "bg-surface"} 
+        ${
+          isVisible &&
+          (slide === "right" ? "animate-slide-right" : "animate-slide-up")
+        }`}
     >
       {children}
     </li>
